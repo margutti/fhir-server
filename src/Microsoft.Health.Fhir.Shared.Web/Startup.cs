5570fea2c44a28afcec137eb9ee3f32c4f2b8672
Brendan Kowitz <bkowitz@microsoft.com>
2019-09-18 16:22:42 -0700

    Sample TableStorage data store

--- src/Microsoft.Health.Fhir.Shared.Web/Startup.cs
+++ src/Microsoft.Health.Fhir.Shared.Web/Startup.cs
@@ -56,6 +56,10 @@ namespace Microsoft.Health.Fhir.Web
                     options.KnownProxies.Clear();
                 });
             }
+            else if (dataStore.Equals(KnownDataStores.TableStorage, StringComparison.InvariantCultureIgnoreCase))
+            {
+                fhirServerBuilder.AddExperimentalTableStorage();
+            }
 
             AddApplicationInsightsTelemetry(services);
         }
